89a953b734e6e851096d9cde0598ddb8807c8287
mperes <miguel@industriallogic.com>
2016-05-21 09:26:25 -0300

    new data format

--- js/bibliography.js
+++ js/bibliography.js
@@ -1,37 +1,26 @@
 bibliography = [
   {
-    cover: 'img/readingList/book1.jpg',
-    title: 'Book 1',
-    resume: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum tortor ipsum, auctor fermentum risus lacinia ullamcorper. Integer pharetra justo vel risus sodales tristique.',
-    categories: {
-      makePeopleAwesome: false,
-      makeSafetyAPrerequisite: false,
-      experimentAndLearnRapidly: false,
-      deliverValueContinuously: false
-    },
-    amazonLink: 'http://www.amazon.com/Drawing-Absolute-Beginner-Clear-Successful/dp/1581807899',
-    downloadLink: null
-  },
-  {
+    thumbnail: null,
     title: 'Badass: Making Users Awesome',
+    resume: null,
     categories: {
       makePeopleAwesome: true,
       makeSafetyAPrerequisite: false,
       experimentAndLearnRapidly: false,
       deliverValueContinuously: false
     },
-    amazonLink: 'http://www.amazon.com/Badass-Making-Awesome-Kathy-Sierra/dp/1491919019',
-    downloadLink: null
+    link: 'http://www.amazon.com/Badass-Making-Awesome-Kathy-Sierra/dp/1491919019'
   },
   {
+    thumbnail: null,
     title: 'Delivering Happiness: A Path to Profits, Passion, and Purpose',
+    resume: null,
     categories: {
       makePeopleAwesome: true,
       makeSafetyAPrerequisite: false,
       experimentAndLearnRapidly: false,
       deliverValueContinuously: false
     },
-    amazonLink: 'http://www.amazon.com/Delivering-Happiness-Profits-Passion-Purpose/dp/160941280X',
-    downloadLink: null
+    link: 'http://www.amazon.com/Delivering-Happiness-Profits-Passion-Purpose/dp/160941280X'
   }
 ];
