cd8b717113a62701f6a9a4060d3488a7a3087175
mperes <miguel@industriallogic.com>
2016-08-08 12:12:35 -0300

    fixed images

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -13,7 +13,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
