eda91a88386b8b7d8e6213dacc7da84e871dc2f3
Tim Ottinger <tottinge@industriallogic.com>
2020-03-30 15:14:46 -0500

    fixed bad wrap, added mob programming cheatsheet

--- data/cheatsheetEntries.js
+++ data/cheatsheetEntries.js
@@ -1,17 +1,5 @@
 cheatsheets=[
   {
-    thumbnail: 'img/merchandise/evolutionary_design_cheat_sheet.jpg',
-    title: 'Evolutionary Design Cheat Sheet',
-    resume: null,
-    categories: {
-      makePeopleAwesome: true,
-      makeSafetyAPrerequisite: true,
-      experimentAndLearnRapidly: true,
-      deliverValueContinuously: true
-    },
-    url: 'https://docs.google.com/document/d/1A1sLe2R-9XhRpOiAZfKii2944PM5tUKS17EgaiGCnLE/export?format=pdf'
-  },
-  {
     thumbnail: 'img/readingList/psychological_safety_cheat_sheet.jpg',
     title: 'Psychological Safety Cheat Sheet',
     resume: null,
@@ -23,18 +11,7 @@ cheatsheets=[
     },
     url: 'https://docs.google.com/document/d/1BAmaH1_U4z5tc3QpV5pMDoPsGmVSVYdSzefIfwjeT7E/export?format=pdf'
   },
-  {
-    thumbnail: 'img/merchandise/accelerating_flow_cheat_sheet.jpg',
-    title: 'Accelerating Flow Cheat Sheet',
-    resume: null,
-    categories: {
-      makePeopleAwesome: true,
-      makeSafetyAPrerequisite: true,
-      experimentAndLearnRapidly: true,
-      deliverValueContinuously: true
-    },
-    url: 'https://docs.google.com/document/d/1a0ortR3E6-DQurlY1s28LwbJBthjAYVtkQQozPxGGVA/export?format=pdf'
-  },
+
   {
     thumbnail: 'img/merchandise/chartering_cheat_sheet.jpg',
     title: 'Chartering Cheat Sheet',
@@ -47,7 +24,35 @@ cheatsheets=[
     },
     url: 'https://docs.google.com/document/d/1s7iO71YYHJ5BPxwoB1W8ywr7KAlO0ozjXCPdXG4nOzg/export?format=pdf'
   },
+
   {
+    thumbnail: 'img/merchandise/accelerating_flow_cheat_sheet.jpg',
+    title: 'Accelerating Flow Cheat Sheet',
+    resume: null,
+    categories: {
+      makePeopleAwesome: true,
+      makeSafetyAPrerequisite: true,
+      experimentAndLearnRapidly: true,
+      deliverValueContinuously: true
+    },
+    url: 'https://docs.google.com/document/d/1a0ortR3E6-DQurlY1s28LwbJBthjAYVtkQQozPxGGVA/export?format=pdf'
+  },
+
+  {
+    thumbnail: 'img/merchandise/evolutionary_design_cheat_sheet.jpg',
+    title: 'Evolutionary Design Cheat Sheet',
+    resume: null,
+    categories: {
+      makePeopleAwesome: true,
+      makeSafetyAPrerequisite: true,
+      experimentAndLearnRapidly: true,
+      deliverValueContinuously: true
+    },
+    url: 'https://docs.google.com/document/d/1A1sLe2R-9XhRpOiAZfKii2944PM5tUKS17EgaiGCnLE/export?format=pdf'
+  },
+
+  ,
+    {
     thumbnail: 'img/readingList/mob_programming_cheat_sheet.jpg',
     title: 'Mob Programming Cheat Sheet',
     resume: null,
