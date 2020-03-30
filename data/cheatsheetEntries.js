22a3fa621a39a774fb3d19666244ffd9269f5c8d
Tim Ottinger <tottinge@industriallogic.com>
2020-03-30 13:41:14 -0500

    Moved cheatsheets above media kit created cheatSheetEntried.js in prep to display LearnMore style removed cheat sheets from LearnMore set video type on both videos (we have type, why not use it?)

---
+++ data/cheatsheetEntries.js
@@ -0,0 +1,62 @@
+cheatsheets=[
+  {
+    thumbnail: 'img/merchandise/evolutionary_design_cheat_sheet.jpg',
+    title: 'Evolutionary Design Cheat Sheet',
+    resume: null,
+    categories: {
+      makePeopleAwesome: true,
+      makeSafetyAPrerequisite: true,
+      experimentAndLearnRapidly: true,
+      deliverValueContinuously: true
+    },
+    url: 'https://docs.google.com/document/d/1A1sLe2R-9XhRpOiAZfKii2944PM5tUKS17EgaiGCnLE/export?format=pdf'
+  },
+  {
+    thumbnail: 'img/readingList/psychological_safety_cheat_sheet.jpg',
+    title: 'Psychological Safety Cheat Sheet',
+    resume: null,
+    categories: {
+      makePeopleAwesome: true,
+      makeSafetyAPrerequisite: true,
+      experimentAndLearnRapidly: true,
+      deliverValueContinuously: true
+    },
+    url: 'https://docs.google.com/document/d/1BAmaH1_U4z5tc3QpV5pMDoPsGmVSVYdSzefIfwjeT7E/export?format=pdf'
+  },
+  {
+    thumbnail: 'img/merchandise/accelerating_flow_cheat_sheet.jpg',
+    title: 'Accelerating Flow Cheat Sheet',
+    resume: null,
+    categories: {
+      makePeopleAwesome: true,
+      makeSafetyAPrerequisite: true,
+      experimentAndLearnRapidly: true,
+      deliverValueContinuously: true
+    },
+    url: 'https://docs.google.com/document/d/1a0ortR3E6-DQurlY1s28LwbJBthjAYVtkQQozPxGGVA/export?format=pdf'
+  },
+  {
+    thumbnail: 'img/merchandise/chartering_cheat_sheet.jpg',
+    title: 'Chartering Cheat Sheet',
+    resume: null,
+    categories: {
+      makePeopleAwesome: true,
+      makeSafetyAPrerequisite: true,
+      experimentAndLearnRapidly: true,
+      deliverValueContinuously: true
+    },
+    url: 'https://docs.google.com/document/d/1s7iO71YYHJ5BPxwoB1W8ywr7KAlO0ozjXCPdXG4nOzg/export?format=pdf'
+  },
+  {
+    thumbnail: 'img/readingList/mob_programming_cheat_sheet.jpg',
+    title: 'Mob Programming Cheat Sheet',
+    resume: null,
+    categories: {
+      makePeopleAwesome: true,
+      makeSafetyAPrerequisite: true,
+      experimentAndLearnRapidly: true,
+      deliverValueContinuously: true
+    },
+    url: 'https://docs.google.com/document/d/1Ve5LVAJvGqJbUZR6C2o3ZNvFPKpuJ6sfIwxlpgKpKCk/export?format=pdf'
+  },
+]
--- data/learnMoreEntries.js
+++ data/learnMoreEntries.js
@@ -12,66 +12,6 @@ learnMoreEntries = [
     url: 'https://www.infoq.com/articles/modern-agile-intro'
   },
   {
-    thumbnail: 'img/merchandise/evolutionary_design_cheat_sheet.jpg',
-    title: 'Evolutionary Design Cheat Sheet',
-    resume: null,
-    categories: {
-      makePeopleAwesome: true,
-      makeSafetyAPrerequisite: true,
-      experimentAndLearnRapidly: true,
-      deliverValueContinuously: true
-    },
-    url: 'https://docs.google.com/document/d/1A1sLe2R-9XhRpOiAZfKii2944PM5tUKS17EgaiGCnLE/export?format=pdf'
-  },
-  {
-    thumbnail: 'img/readingList/psychological_safety_cheat_sheet.jpg',
-    title: 'Psychological Safety Cheat Sheet',
-    resume: null,
-    categories: {
-      makePeopleAwesome: true,
-      makeSafetyAPrerequisite: true,
-      experimentAndLearnRapidly: true,
-      deliverValueContinuously: true
-    },
-    url: 'https://docs.google.com/document/d/1BAmaH1_U4z5tc3QpV5pMDoPsGmVSVYdSzefIfwjeT7E/export?format=pdf'
-  },
-  {
-    thumbnail: 'img/merchandise/accelerating_flow_cheat_sheet.jpg',
-    title: 'Accelerating Flow Cheat Sheet',
-    resume: null,
-    categories: {
-      makePeopleAwesome: true,
-      makeSafetyAPrerequisite: true,
-      experimentAndLearnRapidly: true,
-      deliverValueContinuously: true
-    },
-    url: 'https://docs.google.com/document/d/1a0ortR3E6-DQurlY1s28LwbJBthjAYVtkQQozPxGGVA/export?format=pdf'
-  },
-  {
-    thumbnail: 'img/merchandise/chartering_cheat_sheet.jpg',
-    title: 'Chartering Cheat Sheet',
-    resume: null,
-    categories: {
-      makePeopleAwesome: true,
-      makeSafetyAPrerequisite: true,
-      experimentAndLearnRapidly: true,
-      deliverValueContinuously: true
-    },
-    url: 'https://docs.google.com/document/d/1s7iO71YYHJ5BPxwoB1W8ywr7KAlO0ozjXCPdXG4nOzg/export?format=pdf'
-  },
-  {
-    thumbnail: 'img/readingList/mob_programming_cheat_sheet.jpg',
-    title: 'Mob Programming Cheat Sheet',
-    resume: null,
-    categories: {
-      makePeopleAwesome: true,
-      makeSafetyAPrerequisite: true,
-      experimentAndLearnRapidly: true,
-      deliverValueContinuously: true
-    },
-    url: 'https://docs.google.com/document/d/1Ve5LVAJvGqJbUZR6C2o3ZNvFPKpuJ6sfIwxlpgKpKCk/export?format=pdf'
-  },
-  {
     thumbnail: 'img/readingList/joshagile2016.jpg',
     title: 'Agile 2016: Joshua Kerievsky\'s Keynote',
     resume: null,
@@ -106,6 +46,7 @@ learnMoreEntries = [
       experimentAndLearnRapidly: false,
       deliverValueContinuously: false
     },
+    type: 'video',
     url: 'https://www.youtube.com/watch?v=FF-tKLISfPE'
   },
   {
