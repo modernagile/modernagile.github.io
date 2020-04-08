1ee7911e0f559e4148625b8ab0ebf606229e2e98
Tim Ottinger <tottinge@industriallogic.com>
2020-04-08 11:11:31 -0500

    Tidy up js

--- js/ShowCheatsheets.js
+++ js/ShowCheatsheets.js
@@ -9,16 +9,13 @@ function displayCheatSheets() {
                 <div class="meta">
                 <p class="title">${entry.title}</p>
                 </div><h5>`;
-
         for (source of entry.sources) {
             content = content.concat( `
-            
             <a style="display: inline-block; margin-top: 5px;" href="${source.url}" alt="download and print yourself" target="_blank">
             Download in ${source.language}
             </a><br/>`
             );
         }
-
         content = content.concat(`<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"> 
             <img style="width: 80px; margin-top: 10px; display: inline-block;"  
                 alt="Creative Commons License" 
